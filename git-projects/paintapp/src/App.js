615e643b4b3cdaa7c9f17ad896fc713378d4009a
noueliah <noelittz@gmail.com>
2022-11-18 10:42:15 -0300

    'adding changes

---
+++ git-projects/paintapp/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Playground from "./components/Playground";
+import Name from "./components/Name";
+
+function App() {
+  return (
+    <div>
+      <Name />
+      <Playground />
+    </div>
+  );
+}
+export default App;
